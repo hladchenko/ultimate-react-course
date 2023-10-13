6d739580829fd9d27b6736eac5a3a71a99ba1279
Ruslan Hladchenko <rugladchenko@gmail.com>
2023-10-13 19:28:29 +0300

    Add useEffect and move a message into a new component

--- react-get-advice/src/App.js
+++ react-get-advice/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function App() {
   const [advice, setAdvice] = useState("Advice");
@@ -11,11 +11,19 @@ export default function App() {
     setCount(c => c + 1);
   }
 
+  useEffect(function() {
+    getAdvice();
+  }, []);
+
   return (
     <div>
       <h1>{advice}</h1>
       <button onClick={getAdvice}>Get advice</button>
-      <p>You have read <strong>{count} pieces of advice.</strong></p>
+      <Message count={count} />
     </div>
   );
+}
+
+function Message({count}) {
+  return <p>You have read <strong>{count} pieces of advice.</strong></p>;
 }
